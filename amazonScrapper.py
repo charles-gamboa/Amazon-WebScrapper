503d2b1125dfdbde337569a5fb9889ba2cbb3664
charlesgamboa <charlesgamboa15@gmail.com>
2026-07-15 15:25:29 -0600

    [Feat] fetch_products() Completed
    Got the function to work properly, but does not scrape all products

--- amazonScrapper.py
+++ amazonScrapper.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+import time, random
 
 ######## TYPES ########
 type Product = dict["title": str, "price": float, "discount": int]
@@ -17,39 +17,55 @@ URL = "https://www.amazon.ca/deals"
 def fetch_products(driver: webdriver.Chrome) -> list[Product]:
     ''' 
     Fetches products from the Amazon deals page using the provided WebDriver. Returns a list of products with their title, price, and discount.
+    Args:
+        driver (webdriver.Chrome): The Selenium WebDriver instance used to interact with the Amazon page.
+    Returns:
+        list[Product]: A list of products, where each product is represented as a dictionary containing its title, price, and discount.
     '''
     products: list[Product] = [] # List of products
 
     # Select all product cards on the page
-    product_cards = driver.find_elements(By.CSS_SELECTOR, ".a-color-base.a-spacing-mini.a-link-normal.a-text-normal")
-    #last_count = len(product_cards)
+    product_cards: list[webdriver.Chrome.WebElement] = []
+    last_count = len(product_cards)
 
     # Scroll down the page until no new products are found
-    scroll_height = 0
-    max_scroll_height = driver.execute_script("return document.body.scrollHeight")
+    scroll_delay = 2
+    scroll_step = 0
 
     while True:
+        driver.execute_script("window.scrollTo(arguments[0], arguments[1]);", 0, scroll_step)
         
-        driver.execute_script("window.scrollTo(arguments[0], arguments[1]);", 0, scroll_height)
+        time.sleep(scroll_delay + random.random() * 0.5)
+        
+        product_cards = driver.find_elements(By.CSS_SELECTOR, ".a-color-base.a-spacing-mini.a-link-normal.a-text-normal") # Select all product cards
 
-        time.sleep(2) # Wait for JS to load
-        
-        product_cards = driver.find_elements(By.CSS_SELECTOR, ".a-color-base.a-spacing-mini.a-link-normal.a-text-normal") # Re-select all product cards
-        #new_count = len(product_cards)
+        new_count = len(product_cards)
 
-        if scroll_height >= max_scroll_height:
-            break
-        
-        scroll_height += 1000
-        #last_count = new_count
+        # Re-try once more
+        if new_count <= last_count:
+            last_count = new_count
+            scroll_step += 500 + random.random() * 500
+
+            driver.execute_script("window.scrollTo(arguments[0], arguments[1]);", 0, scroll_step)
+            
+            time.sleep(scroll_delay + random.random() * 0.5)
+
+            product_cards = driver.find_elements(By.CSS_SELECTOR, ".a-color-base.a-spacing-mini.a-link-normal.a-text-normal")
+            new_count = len(product_cards)
+
+            if new_count <= last_count:
+                break
+
+        last_count = new_count
+        scroll_step += 1000 + random.random() * 500
 
     # Select all public data from each product card
     for product_card in product_cards:
         title_element = product_card.find_element(By.CSS_SELECTOR, ".a-truncate-cut")
         price_whole_element = product_card.find_element(By.CSS_SELECTOR, ".a-price-whole")
         price_fraction_element = product_card.find_element(By.CSS_SELECTOR, ".a-price-fraction")
-        discount_element = product_card.find_element(By.CSS_SELECTOR, ".style_filledRoundedBadgeLabel__Vo").find_element(By.CSS_SELECTOR, ".a-size-mini")
-
+        discount_element = driver.find_element(By.XPATH, "//span[contains(text(), 'off')]")
+    
         title = title_element.text
         price = float(price_whole_element.text.replace("$", "").replace(",", "") + "." + price_fraction_element.text)
         discount = int(discount_element.text[0:2])
@@ -60,7 +76,7 @@ def fetch_products(driver: webdriver.Chrome) -> list[Product]:
 
 ######## MAIN ########
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")  # Run without opening a window
+options.add_argument("--headless")  # Run without opening a window
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
@@ -72,7 +88,5 @@ driver.get(URL)
 time.sleep(1) # Wait for JS to load
 
 products = fetch_products(driver) # Fetch products from the Amazon url page
-
-print(products)
 
 driver.quit() # Exit driver
