4fc8d5e5a8cace3c7fc0c0641b439ee735e2da72
charlesgamboa <charlesgamboa15@gmail.com>
2026-07-14 23:12:52 -0600

    [Chore] Setup WebDriver
    Completed the setup for the webdriver

--- amazonScrapper.py
+++ amazonScrapper.py
@@ -6,6 +6,26 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-######## CONSTANTS ########
+######## TYPES ########
+type Product = dict["title": str, "price": float, "discount": int]
 
-######### METHODS ########
+######## CONSTANTS ########
+URL = "https://www.amazon.ca/deals"
+
+######## METHODS ########
+
+def fetch_products() -> list[Product]:
+    products: list[Product] = []
+
+######## MAIN ########
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")  # Run without opening a window
+options.add_argumentq("--disable-blink-features=AutomationControlled")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# Initialize the Chrome WebDriver with the specified options
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get(URL)
+
+time.sleep(3) # Wait for JS to load
